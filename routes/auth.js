349d5c5ccd50cde60c25ae5f5f073022b9fbd569
Teoalmonte <teoalmonte@gmail.com>
2018-03-09 18:40:37 -0500

    still working on auth

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const mongodb = require('mongodb').MongoClient;
+var passport = require('passport');
 
-router.post('/signUp', (req, res) => {
-  console.log(`the ${req.body.username}`);
+router.post('/signUp',function(req, res){
+  // console.log(req.body);
   req.login(req.body, function () {
     res.redirect('/auth/profile');
   });
 });
 
 router.get('/profile', (req, res) => {
-  res.json(req.body);
-
-
+  console.log(`profile page ${req.body}`);
+  res.json(req.user)
 });
+
 
 module.exports = router;
