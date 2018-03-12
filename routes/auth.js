c8b687cf1239d9f27ca368c2ddcad7a0a655d038
Teo Almonte <teoalmonte@gmail.com>
2018-03-11 22:17:31 -0400

    new

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,20 @@ const mongodb = require('mongodb').MongoClient;
 
 router.post('/signUp', (req, res) => {
   console.log(req.body)
-  req.login(req.body, () => {
-    res.redirect('/auth/profile')
-  })
+  const url = 'mongodb://localhost:27017/libapp';
+  mongodb.connect(url, (err, client) => {
+    let db = client.db('libapp');
+    let collection = db.collection('users');
+      let user = {
+        username: req.body.username,
+        password: req.body.password
+      };
+      collection.insert(user, (err, results) => {
+        req.login(results.ops[0], () => {
+          res.redirect('/auth/profile')
+        })
+      })
+    })
 })
 
 router.get('/profile', (req, res) => {
